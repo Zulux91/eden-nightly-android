d7cd31713fd13a4f4c86fab5f37e0f3e522b04eb
Zulux91 <leodiaz91.ld@gmail.com>
2025-07-01 03:34:35 -0500

    Build core

---
+++ eden-android.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -ex
+
+# Clone Eden, fallback to mirror if upstream repo fails to clone
+if ! git clone 'https://git.eden-emu.dev/eden-emu/eden.git' ./eden; then
+	echo "Using mirror instead..."
+	rm -rf ./eden || true
+	git clone 'https://github.com/pflyly/eden-mirror.git' ./eden
+fi
+
+cd ./eden
+git submodule update --init --recursive
+
+# workaround for prebuilt ffmpeg download failure
+# use prebuilt ffmpeg from https://git.eden-emu.dev/eden-emu/ext-android-bin
+# content unchanged
+sed -i 's|set(package_base_url "https://git.eden-emu.dev/eden-emu/")|set(package_base_url "https://github.com/pflyly/eden-nightly/")|' CMakeModules/DownloadExternals.cmake
+sed -i 's|set(package_repo "ext-android-bin/raw/master/")|set(package_repo "raw/refs/heads/main/")|' CMakeModules/DownloadExternals.cmake
+
+if [ "$TARGET" = "Coexist" ]; then
+    # Change the App name and application ID to make it coexist with official build
+    sed -i 's/applicationId = "dev\.eden\.eden_emulator"/applicationId = "dev.eden.eden_emulator.nightly"/' src/android/app/build.gradle.kts
+    sed -i 's/resValue("string", "app_name_suffixed", "eden")/resValue("string", "app_name_suffixed", "eden unofficial")/' src/android/app/build.gradle.kts
+    sed -i 's|<string name="app_name"[^>]*>.*</string>|<string name="app_name" translatable="false">Eden Unofficial</string>|' src/android/app/src/main/res/values/strings.xml
+fi        
+
+if [ "$TARGET" = "Optimised" ]; then
+    # Add optimised to the app name
+    sed -i 's/resValue("string", "app_name_suffixed", "eden")/resValue("string", "app_name_suffixed", "eden optimised")/' src/android/app/build.gradle.kts
+    sed -i 's|<string name="app_name"[^>]*>.*</string>|<string name="app_name" translatable="false">Eden Optimised</string>|' src/android/app/src/main/res/values/strings.xml
+fi 
+
+COUNT="$(git rev-list --count HEAD)"
+APK_NAME="Eden-${COUNT}-Android-Unofficial-${TARGET}"
+
+cd src/android
+chmod +x ./gradlew
+if [ "$TARGET" = "Optimised" ]; then
+	./gradlew assembleGenshinSpoofRelease --console=plain --info -Dorg.gradle.caching=true
+else
+	./gradlew assembleMainlineRelease --console=plain --info -Dorg.gradle.caching=true
+fi
+
+APK_PATH=$(find app/build/outputs/apk -type f -name "*.apk" | head -n 1)
+if [ -z "$APK_PATH" ]; then
+    echo "Error: APK not found in expected directory."
+    exit 1
+fi
+mkdir -p artifacts
+mv "$APK_PATH" "artifacts/$APK_NAME.apk"
