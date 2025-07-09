fd99ac2e1caaab9cc977a9fb61231a6c671b33a6
Zulux91 <leodiaz91.ld@gmail.com>
2025-07-09 17:57:56 -0500

    Update changelog script to use correct compare URL and fix changelog entry format

--- changelog.sh
+++ changelog.sh
@@ -24,7 +24,7 @@ echo "$COUNT" > ~/count
 # Start to generate release info and changelog
 CHANGELOG_FILE=~/changelog
 BASE_COMMIT_URL="https://git.eden-emu.dev/eden-emu/eden/commit"
-BASE_COMPARE_URL="https://github.com/pflyly/eden-mirror/compare"
+BASE_COMPARE_URL="https://git.eden-emu.dev/eden-emu/eden/compare"
 BASE_DOWNLOAD_URL="https://github.com/Zulux91/eden-nightly-android/releases/download"
 
 # Fallback if OLD_HASH is empty or null
@@ -55,14 +55,14 @@ git log --reverse --pretty=format:"%H %s" "${OLD_HASH}..HEAD" | while IFS= read 
   i=$((i + 1))
 done
 
-# Add full changelog from lastest official tag release
-RELEASE_TAG="$(git describe --tags | awk -F'-' '{print $1 "-" $2 "-" $3}')"
-echo "Full Changelog: [\`${RELEASE_TAG}...master\`](${BASE_COMPARE_URL}/${RELEASE_TAG}...master)" >> "$CHANGELOG_FILE"
+# Add full changelog from lastest official tag release (temp fix with 0.0.2-pre-alpha tag)
+# RELEASE_TAG="$(git describe --tags | awk -F'-' '{print $1 "-" $2 "-" $3}')"
+echo "Full Changelog: [\`0.0.2-pre-alpha...master\`](${BASE_COMPARE_URL}/0.0.2-pre-alpha...master)" >> "$CHANGELOG_FILE"
 echo >> "$CHANGELOG_FILE"
 
 # Generate release table
 echo "## Unofficial Nightly Release: ${COUNT}" >> "$CHANGELOG_FILE"
-echo "| Platform | Target / Arch | |" >> "$CHANGELOG_FILE"
+echo "| Platform | Target / Arch |" >> "$CHANGELOG_FILE"
 echo "|--|--|--|" >> "$CHANGELOG_FILE"
 echo "| Android | [\`Coexist\`](${BASE_DOWNLOAD_URL}/${TAG}/Eden-${COUNT}-Android-Unofficial-Coexist.apk)<br><br>[\`Replace\`](${BASE_DOWNLOAD_URL}/${TAG}/Eden-${COUNT}-Android-Unofficial-Replace.apk)<br><br>[\`Optimised\`](${BASE_DOWNLOAD_URL}/${TAG}/Eden-${COUNT}-Android-Unofficial-Optimised.apk) |" >> "$CHANGELOG_FILE"
 echo "| Any | [Source Code](${BASE_DOWNLOAD_URL}/${TAG}/Eden-${COUNT}-Source-Code.7z) |" >> "$CHANGELOG_FILE"
